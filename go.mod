1596006eb372f7cb939303c3bea264da6281f907
dirk-w85 <dirk.woellhaf@gmail.com>
2021-07-13 19:30:13 +0200

    TOML Test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module te-cos
+
+go 1.16
+
+require github.com/pelletier/go-toml v1.9.3 // indirect
---
+++ te-cos_dev.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	//"fmt"
+	"log"
+	//"net/http"
+	//"io/ioutil"
+	//"encoding/json"
+	//"time"  
+	//"encoding/base64"
+	"github.com/pelletier/go-toml"
+)
+
+
+func ErrorCheck(e error) {
+    if e != nil {
+		log.Fatalln(e)
+        panic(e)
+    }
+}
+
+
+func main() {
+	config, _ := toml.LoadFile("config.toml")
+	// retrieve data directly
+	user := config.Get("account.token").(string)
+	log.Println(user)
+}
